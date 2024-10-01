ca2340eee2d20211a9aea1f8456d4cbcb2ef0ed1
Maksim Tsapaev <drfausto@mail.ru>
2024-10-01 16:10:16 +0400

    Adding GPT

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 from email.policy import default
+from types import NoneType
 
 from telegram.ext import ApplicationBuilder, MessageHandler, filters, CallbackQueryHandler, CommandHandler
 
@@ -6,20 +7,32 @@ from gpt import *
 from util import *
 
 async def start(update, context):
+    dialog.mode = 'main'
     text = load_message('main')
     await send_photo(update, context, 'main')
     await send_text(update, context, text)
 
+async def gpt(update, context):
+    dialog.mode= 'gpt'
+    text = load_message('gpt')
+    await send_photo(update, context, 'gpt')
+    await send_text(update, context, text)
+
+async def gpt_dialog(update, context):
+    await send_text(update, context, 'вы общаетесь с чатом GPT')
 
 # тут будем писать наш код :)
 async def hello(update, context):
-    await send_text(update, context, '*Привет*')
-    await send_text(update, context, '_Как дела?_')
-    await send_text(update, context, 'Вы написали '+ update.message.text)
-    await send_photo(update,context, 'avatar_main')
-    await send_text_buttons(update, context, 'Запустить процесс?', {
-        'start':'Запустить',
-        'stop':'Остановить'
+    if dialog.mode == 'gpt':
+        await gpt_dialog(update, context)
+    else:
+        await send_text(update, context, '*Привет*')
+        await send_text(update, context, '_Как дела?_')
+        await send_text(update, context, 'Вы написали '+ update.message.text)
+        await send_photo(update,context, 'avatar_main')
+        await send_text_buttons(update, context, 'Запустить процесс?', {
+            'start':'Запустить',
+            'stop':'Остановить'
     } )
 
 async def hello_button(update, context):
@@ -30,8 +43,15 @@ async def hello_button(update, context):
         await send_text(update, context, 'Процесс остановлен')
 
 
-app = ApplicationBuilder().token("7676179726:AAGuucXEJwfLUejNxrzU6Xwk8rsEsph1ogc").build()
+dialog = Dialog()
+dialog.mode = None
+
+chatgpt = ChatGptService(token='6iWpGTIVtrvZB0KTFlGqJFkblB3Tjikl0aobFUM6zUmVCkXU')
+
+
+app = ApplicationBuilder().token("7676179726:AAEzXm-UJFumBkOJT1HYLX5q9W2QOiLzYsc").build()
 app.add_handler(CommandHandler('start', start))
+app.add_handler(CommandHandler('gpt', gpt))
 app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND,hello))
 app.add_handler(CallbackQueryHandler(hello_button))
 app.run_polling()
